a921c2e32f21bd904025724ee51e1015de5cd399
PrasadVagger <prasadkv6677@gmail.com>
2024-12-03 12:53:59 +0530

    tyjg

--- test2/Model/NameModel.cs
+++ test2/Model/NameModel.cs
@@ -3,8 +3,10 @@
     public class NameModel
     {
         public int Id { get; set; } = 0; // Default to 0
-        public string Name { get; set; } = ""; // Default to empty string
+        public string Name { get; set; } // Default to empty string
         public int Age { get; set; } = 0; // Default to 0
+        public string City { get; set; }
+
     }
 
 }
